a9bf766e3fd85957c8fa2a9f35d0f3fdd1f122a8
Dhananjay-Jaipur <mr.vip82095@gmail.com>
2024-08-29 17:06:43 +0530

    new

---
+++ matharea/area.py
@@ -0,0 +1,13 @@
+# area.py
+'''calculate area'''
+
+def rectangle_area(l,b):
+    "rectangle area"
+    return l * b
+
+def circle_area(r):
+    "circle area"
+    return p * r * r
+
+# constants used
+p = 3.14
